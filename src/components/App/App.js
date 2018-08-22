aea73f413558e4c3ddf8709858c4854ffcaa8d5c
Sage Vogt <thurmanvogt@gmail.com>
2018-08-22 13:24:49 -0600

    Add a tags to link to gigsmart and github

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,9 +7,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="menu-button-of-three-horizontal-lines.png" className="Menu-icon" />
-          <img src="gigsmart-footer-logo.png" className="App-logo"/>
-          <img src="man-user.png" className="User-icon" />
+          <a href="">
+            <img src="menu-button-of-three-horizontal-lines.png" className="Menu-icon" />
+          </a>
+          <a href="https://gigsmart.com/">
+            <img src="gigsmart-footer-logo.png" className="App-logo"/>
+          </a>
+          <a href="https://github.com/SageVanGogt">
+            <img src="man-user.png" className="User-icon" />
+          </a>
         </header>
         <Job/>
       </div>
