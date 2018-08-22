d7eb30467886dc58fb6ca48029db1ce78b0014fd
Sage Vogt <thurmanvogt@gmail.com>
2018-08-22 00:43:50 -0600

    Add content to dropdowns

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -24,15 +24,23 @@ class Job extends Component {
               <h3>Skills</h3>
             </AccordionItemTitle>
             <AccordionItemBody>
-              <p>Body content</p>
+              <p>Javascript, React, Redux, Jest, Enzyme, CSS, SASS, HTML, Node, Express, RESTful API design</p>
             </AccordionItemBody>
           </AccordionItem>
           <AccordionItem>
             <AccordionItemTitle>
-              <h3>Complex title</h3>
+              <h3>About Me</h3>
             </AccordionItemTitle>
             <AccordionItemBody>
-              <p>Body content</p>
+              <p>Recent grad of Colorado College, avid mountain biker, climber, and innovation enthusiast. Does not like long walks on the beach.</p>
+            </AccordionItemBody>
+          </AccordionItem>
+          <AccordionItem>
+            <AccordionItemTitle>
+              <h3>Availability</h3>
+            </AccordionItemTitle>
+            <AccordionItemBody>
+              <p>Currently available for full time employment</p>
             </AccordionItemBody>
           </AccordionItem>
         </Accordion>
